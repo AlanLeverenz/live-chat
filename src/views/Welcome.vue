8fffc846c9df0c6ce32a7af62e997ec521eb90eb
Alan Leverenz <awleverenz@aol.com>
2021-05-02 22:11:10 -0400

    added Login form

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
-    <SignupForm />
+    <!-- <SignupForm /> -->
+    <LoginForm />
   </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm'
+import LoginForm from '../components/LoginForm'
 
 export default {
   // register the imported component
-  components: { SignupForm }
+  components: { SignupForm, LoginForm }
 }
 </script>
 
@@ -18,5 +20,23 @@ export default {
   .welcome {
     text-align: center;
     padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
   }
 </style>
