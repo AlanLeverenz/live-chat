c07d72def516b3d619d69ca099215f46b0377c0b
Alan Leverenz <awleverenz@aol.com>
2021-05-02 21:44:57 -0400

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore/'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfqbiHJKcMEd9ryWNmJizm3I_tLObs_iM",
+    authDomain: "vue-firebase-sites-bf315.firebaseapp.com",
+    projectId: "vue-firebase-sites-bf315",
+    storageBucket: "vue-firebase-sites-bf315.appspot.com",
+    messagingSenderId: "248777487074",
+    appId: "1:248777487074:web:396ca930aa6278c6fb24bb"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
