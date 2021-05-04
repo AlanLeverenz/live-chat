d831c893c5d5d80f4a10726a15dfbdda63cc690a
Alan Leverenz <awleverenz@aol.com>
2021-05-04 16:31:31 -0400

    updated profile with displayName

--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -10,7 +10,13 @@ const signup = async (email, password, displayName) => {
         if(!res) {
             throw new Error('Could not complete the signup')
         }
+        await res.user.updateProfile({ displayName })
+        error.value = null
+
         console.log(res.user)
+
+        return res
+
     } catch(err) {
         console.log(err.message)
         error.value = err.message
